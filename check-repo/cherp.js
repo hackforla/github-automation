fb9e309c3b6cc7589019032e557dda1a1d55ce6a
jafow <jared.a.fowler@gmail.com>
2020-06-27 21:43:50 -0700

    move cli to root

--- check-repo/cherp.js
+++ check-repo/cherp.js
@@ -13,15 +13,16 @@ async function main () {
     throw Error('Missing GITHUB_ORG environment variable')
   }
   const cherp = new Cherp({ userAgent: process.env.USER_AGENT || '%20%09%55%2b%31%46%34%32%36 cherp', githubOrg: process.env.GITHUB_ORG })
+
   if (argv._[0] === 'license') {
     let repos = await cherp.listAllReposMissingLicense()
     LOGGER.info(repos)
-    process.exit(0)
   } else if (argv._[0] === 'add-file') {
-    cherp.addFile(argv)
+    await cherp.addFile(argv)
   } else {
     usage()
   }
+  process.exit(0)
 }
 
 main()
